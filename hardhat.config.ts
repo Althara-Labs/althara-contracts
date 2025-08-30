8ee39e099c17d2d34c00713e5f12940c15f481da
Fabian Sanchez <fabiansanchezd@outlook.com>
2025-08-30 12:05:26 -0600

    for deployment of smart contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,8 +24,10 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    hardhat: {
-      type: "edr-simulated"
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://sepolia.infura.io/v3/YOUR_INFURA_API_KEY",
+      accounts: [process.env.PRIVATE_KEY || "YOUR_PRIVATE_KEY"],
     },
   },
 };
